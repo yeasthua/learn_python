6fdb73f53da28469e4b3811458458a675e62d6c9
yeshua <atrionsyeshua@gmail.com>
2026-02-13 21:01:32 +0800

    Printing using f-strings and newlines

---
+++ day01/strings.py
@@ -0,0 +1,21 @@
+# Printing with F-Strings
+
+
+name = "Yeasthua"
+age = 18
+skill1 = "python"
+level1 = "beginner"
+skill2 = "c++"
+level2 = "beginner"
+skill3 = "programming"
+level3 = "noobie"
+lower = 20000
+upper = 50000
+
+print(f"my name is {name}, I am {age} years old\n")
+
+print(f"my skills are")
+print(f" - {skill1} ({level1})")
+print(f" - {skill2} ({level2})")
+print(f" - {skill3} ({level3})\n")
+print(f"I am looking for a job with a salary of {lower}-{upper} pesos per month")
