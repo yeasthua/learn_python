5ad10e525da24c6b2fec54a848262635bcf3c1de
yeshua <atrionsyeshua@gmail.com>
2026-02-13 23:05:44 +0800

    Prints the number of seconds in whatever how many days the user inputs

---
+++ day01/arithmetics.py
@@ -0,0 +1,25 @@
+# Converts input into int and use it to perform arithmetic operations
+num_input = int(input("Please type in a number: "))
+print(f"{num_input} times 5 is {num_input * 5}")
+
+
+# A program which asks for a name and a birth year and calculates it's age at the end of 2021
+name = input("What is your name? ")
+birthyear = int(input("Which year were you born? "))
+
+print(f"Hi {name}, you will be {2021 - birthyear} years old at the end of the year 2021")
+
+
+#Prints the minutes in a year using arithmetic operators
+print(365 * 24 * 60)    
+#There are 365 days in a year multiplied (*) by 24 hours (1 day) and 60 minutes (1 hour)
+
+
+#Prints the number of seconds in whatever how many days the user inputs
+days = int(input("How many days? "))
+print(f"Seconds in that many days: {days * 24 * 60 * 60}")
+# variable {days} x 24 hours x 60 mins x 60 seconds
+
+
+
+
--- day01/minutes_in_a_year.py
+++
@@ -1,3 +0,0 @@
-#Prints the minutes in a year using arithmetic operators
-
-print(365 * 24 * 60)     #There are 365 days in a year multiplied (*) by 24 hours (1 day) and 60 minutes (1 hour)
