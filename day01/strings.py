979df0f1fbf57c2fe9f4225e345cf027fbc7e960
yeshua <atrionsyeshua@gmail.com>
2026-02-13 21:42:03 +0800

    Printing using end= to remove newlines

--- day01/strings.py
+++ day01/strings.py
@@ -27,3 +27,13 @@ print(f"{x} + {y} = {x + y}")
 print(f"{x} - {y} = {x - y}")
 print(f"{x} * {y} = {x * y}")
 print(f"{x} / {y} = {x / y}")
+
+
+# Printing using end="" to remove newlines
+print(5, end="")
+print(" + ", end="")
+print(8, end="")
+print(" - ", end="")
+print(4, end="")
+print(" = ", end="")
+print(5 + 8 - 4, end="")
