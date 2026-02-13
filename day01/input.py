2d0d277f383d891839a3add556d96b4c248e04e3
yeshua <atrionsyeshua@gmail.com>
2026-02-13 19:23:57 +0800

    Concatenating variable input and a text into a single string

--- day01/input.py
+++ day01/input.py
@@ -3,3 +3,7 @@
 
 student = input("Enter your name: ")
 print("Oh hello there! " + student)
+
+# Concatenating a variable and a text into a single string
+name = input("What is your name? ")
+print("!" + name + "!" + name + "!")
