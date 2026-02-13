18956b308b5c52cfde32611891d7b4b7ab80df69
yeshua <atrionsyeshua@gmail.com>
2026-02-13 19:32:05 +0800

    Using multiple variables for storing multiple inputs

--- day01/input.py
+++ day01/input.py
@@ -1,9 +1,18 @@
 # Prompts the user to type and stores it to variable "student"
 # then prints the variable
-
 student = input("Enter your name: ")
 print("Oh hello there! " + student)
 
 # Concatenating a variable and a text into a single string
 name = input("What is your name? ")
 print("!" + name + "!" + name + "!")
+
+# Using multiple variables for storing multiple inputs
+name = input("Given name: ")
+family_name = input("Family name: ")
+address = input("Street address: ")
+postal_code = input("City and postal code: ")
+
+print(name + " " + family_name)
+print(address)
+print(postal_code)
