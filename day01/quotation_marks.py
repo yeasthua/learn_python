7b4d83e5984dcdc12be1ba81f5bccadef5461050
yeshua <atrionsyeshua@gmail.com>
2026-02-13 18:54:33 +0800

    Printing quotation marks

---
+++ day01/quotation_marks.py
@@ -0,0 +1,5 @@
+# Since python accepts double "" or single '' quotation marks to print a string
+# This is handy in case you need to print out the quotation marks!
+
+
+print('print("Hello there!")')      # Outputs print("Hello there!")
