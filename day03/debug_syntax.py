6da05e17b47b901cef2fd4607f2a36bf63080f64
yeshua <atrionsyeshua@gmail.com>
2026-03-13 22:35:46 +0800

    Syntax debugging practice

--- day03/debug_syntax.py
+++ day03/debug_syntax.py
@@ -1,9 +1,9 @@
 number = int(input("Please type in a number: "))
 
-print( "condition: ", number > 100)         # Checks if the number inputted is greater than 100
+print( "condition: ", number > 100)         # [DEBUG] Checks if the number inputted is greater than 100
 if number > 100:
     print("The number was greater than one hundred")
-    print("Original number: ", number)      # Prints the original number
+    print("Original number: ", number)      # [DEBUG] Prints the original number
     number -= 100
     print("Now its value has decreased by one hundred")
     print("Its value is now", number)       # Prints the number decreased by 100
