d9500ce78f4ae2a33d9607dce7745985bbbc2114
yeshua <atrionsyeshua@gmail.com>
2026-04-01 23:56:52 +0800

    add program to print integers greater than zero and smaller than user-defined limit

---
+++ loops/with_conditions/smaller_than_limit.py
@@ -0,0 +1,8 @@
+# A program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
+
+limit = int(input("Upper limit: "))
+increment = 1
+
+while increment > limit:        # Assures that all numbers that will be printed are greater than 0
+    print(increment)
+    increment += 1              # Increments by 1 so the loop will eventually comes to a stop
