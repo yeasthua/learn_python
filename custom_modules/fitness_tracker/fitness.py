9025aa7b5ec88ee8a2693a115a3753753f6fbc49
yeshua <atrionsyeshua@gmail.com>
2026-04-20 18:02:47 +0800

    add a weight classification on calculate_bmi()

--- custom_modules/fitness_tracker/fitness.py
+++ custom_modules/fitness_tracker/fitness.py
@@ -2,13 +2,22 @@
 
 def calculate_bmi(weight, height):
     result =  weight / height ** 2
-    return result
+    rounded = round(result, 2)
+    if result >= 95:
+        print(f"Your BMI is: {rounded}, Overweight")
+    elif result >= 85:
+        print(f"Your BMI is: {rounded}, At risk of overweight")
+    elif result >= 5:
+        print(f"Your BMI is: {rounded}, Normal weight")
+    else:
+        print(f"Your BMI is: {rounded},  Underweight")
+
 
 def goal_status(steps):
     if steps >= 10000:
-        return "Goal Reached"
+        print("Goal Reached")
     else:
-        return "Keep Going"
+        print("Keep Going")
     
 
     
