ecc8c9d4922bc10b84088c2bdbd69437a6f8f7d7
yeshua <atrionsyeshua@gmail.com>
2026-04-20 19:48:58 +0800

    refactor: clarify comment for factorial calculation

--- modules/math_module.py
+++ modules/math_module.py
@@ -5,7 +5,7 @@ num = float(input("Enter a number: "))
 sq_root = math.sqrt(num)        # computes the square root of a number
 ceiling = math.ceil(num)        # rounds the number UP to the nearest whole number
 flr = math.floor(num)           # rounds the number DOWN to the nearest whole number
-fact = math.factorial(int(num)) # computes the product of all positive integers up to num
+fact = math.factorial(int(num)) # converts float to int and computes the product of all positive integers up to num
 
 # Displays the variables onto the console
 print(f"Square root: {round(sq_root, 3)}")
