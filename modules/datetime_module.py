40adc1e5b59efad83651f211e4cea7071fb91125
yeshua <atrionsyeshua@gmail.com>
2026-04-20 20:56:03 +0800

    refactor: simplify datetime module

--- modules/datetime_module.py
+++ modules/datetime_module.py
@@ -1,11 +1,11 @@
-import datetime  #  built-in datetime module to perform date calculations.
+from datetime import datetime  #  built-in datetime module to perform date calculations.
 
 # Converts the user's input string (e.g. "2004-05-21") into a datetime object so it can be subtracted to today's date
 # strptime() strips the string using the format YYYY-MM-DD translated into "%Y-%m-%d" using format specifiers
-birthdate = datetime.datetime.strptime(input("Input your birthdate (YYYY-MM-DD): "), "%Y-%m-%d")
+birthdate = datetime.strptime(input("Input your birthdate (YYYY-MM-DD): "), "%Y-%m-%d")
 
 # Subtracts the {birthdate} from today's date, resulting in a timedelta object
-current_age = datetime.datetime.today() - birthdate
+current_age = datetime.today() - birthdate
 
 # .days extracts the total number of days from the timedelta object inside the datetime module
 # // 365 performs floor division to convert days into whole years
